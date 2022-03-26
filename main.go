e3793dbd43187f4ca7c0fb5b6b5d92dbbd1bbd0e
Deepak Guptha <vzan2012@gmail.com>
2022-03-27 00:27:53 +0530

    feat: Updated with var and const

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main( ) {
-	fmt.Println("Welcome to Our Booking Application")
+	var conferenceName = "Go Lang Conference"
+	const conferenceTickets = 50;
+	var remainingTickets = 50;
+
+	fmt.Printf("Welcome to Our %v - Booking Application \n",conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available !!! \n" ,conferenceTickets,remainingTickets)
 	fmt.Println("Get tickets to attend the meeting")
 }
