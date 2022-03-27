b5749c5e0ca04f68d87f562af6e569d827a4e86b
Deepak Guptha <vzan2012@gmail.com>
2022-03-27 21:22:11 +0530

    feat: Added Booking len - arrays and slices

--- main.go
+++ main.go
@@ -3,11 +3,37 @@ package main
 import "fmt"
 
 func main( ) {
-	var conferenceName = "Go Lang Conference"
-	const conferenceTickets = 50;
-	var remainingTickets = 50;
+	var conferenceName string = "Go Lang Conference"
+	const conferenceTickets int = 50;
+	var remainingTickets uint = 50;
+	bookings := [] string{}
 
 	fmt.Printf("Welcome to Our %v - Booking Application \n",conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available !!! \n" ,conferenceTickets,remainingTickets)
 	fmt.Println("Get tickets to attend the meeting")
+
+	// Data Types 
+	var firstName string;
+	var lastName string;
+	var userTickets uint;
+	var emailAddress string
+
+	fmt.Println("Enter your First Name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your Last Name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter number of tickets required:")
+	fmt.Scan(&userTickets)
+	fmt.Println("Enter your Email Address:")
+	fmt.Scan(&emailAddress)
+
+	remainingTickets -= userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Bookings: %v \n", bookings[0])
+	fmt.Printf("Bookings (Type): %T \n", bookings)
+	fmt.Printf("Bookings (Length): %v \n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email to %v \n", firstName,lastName, userTickets, emailAddress);
+	fmt.Printf("%v remaining tickets for %v", remainingTickets, conferenceName)
 }
